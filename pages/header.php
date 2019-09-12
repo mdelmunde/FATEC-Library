9e2a980cb4c5ac7114e888f67386e230b09b8fcf
Vinicius Vieira <51539646+vieiravini@users.noreply.github.com>
2019-09-12 14:22:21 -0300

    Add files via upload
    updated all the pages to php

---
+++ pages/header.php
@@ -0,0 +1,8 @@
+<header>
+        <div class="title">Biblioteca - FATEC</div>
+      <div class="user"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/peterme/48.jpg"/>
+        <div class="content">
+          <div class="name">Vinicius Vieira</div>
+        </div>
+      </div>
+    </header>
---
+++ pages/nav.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+            <ul>
+              <li>Cadastro de livros</li>
+              <li>Cadastro de alunos</li>
+              <li>Imprestimo</li>
+              <li>Devolução</li>
+              <li>Consulta</li>
+              <li>Histórico</li>
+              <li>Perfil</li>
+            </ul>
+          </div>
